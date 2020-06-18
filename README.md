e64e665e515fde638f489e6c1b130cf1faa24f0e
anton <antonkotik0@gmail.com>
2020-06-18 18:28:25 +0300

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+# react-native-airplay-btn
+AirPlay library for iOS
+
+## Installation with Automatic Linking
+```js
+npm i react-native-airplay-btn --save
+react-native link
+```
+
+### How to create listeners
+
+```js
+import { AirPlayListener } from react-native-airplay-btn
+
+this.airPlayAvailable = AirPlayListener.addListener('airplayAvailable', devices => this.setState({
+      airPlayAvailable: devices.available,
+})); --> returns a boolean
+
+this.airPlayConnected = AirPlayListener.addListener('airplayConnected', devices => this.setState({
+      airPlayConnected: devices.connected,
+})); --> returns a boolean
+
+
+// Remove Listeners in componentWillUnmount
+this.airPlayConnected.remove();
+this.airPlayAvailable.remove()
+
+```
+
+## Methods
+
+```js
+  AirPlay.startScan();
+  
+  AirPlay.disconnect();
+```
+
+### Create AirPlay Button
+
+```js
+import { AirPlayButton } from 'react-native-airplay-btn';
+
+<Button style={{ height: 30, width: 30, justifyContent: 'center', alignItems:'center' }} />
+```
+
+Note: The AirPlay Button does not show in the simulator
+
+
+## Author
+
+Nadia Dillon
---
+++ ios/RNAirplay.m
@@ -0,0 +1,85 @@
+#import "RNAirplay.h"
+#import "RNAirplayManager.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+
+@implementation RNAirplay
+@synthesize bridge = _bridge;
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(startScan)
+{
+    printf("init Airplay");
+    AVAudioSessionRouteDescription* currentRoute = [[AVAudioSession sharedInstance] currentRoute];
+    BOOL isAvailable = false;
+    int routeCount = (int)[[currentRoute outputs] count];
+    BOOL isSpeaker = ([[[[currentRoute outputs] objectAtIndex:0] portName] isEqualToString:@"Speaker"]);
+    if(routeCount > 0 && !isSpeaker) {
+        isAvailable = true;
+        BOOL isConnected = true;
+        for (AVAudioSessionPortDescription * output in currentRoute.outputs) {
+            if([output.portType isEqualToString:AVAudioSessionPortAirPlay]) {
+                [self sendEventWithName:@"airplayConnected" body:@{@"connected": @(isConnected)}];
+            }
+        }
+        [[NSNotificationCenter defaultCenter]
+         addObserver:self
+         selector: @selector(airplayChanged:)
+         name:AVAudioSessionRouteChangeNotification
+         object:[AVAudioSession sharedInstance]];
+    }
+    
+    [self sendEventWithName:@"airplayAvailable" body:@{@"available": @(isAvailable)}];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self isAvailable];
+    });
+}
+
+RCT_EXPORT_METHOD(disconnect)
+{
+    printf("disconnect Airplay");
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [self sendEventWithName:@"airplayAvailable" body:@{@"available": @(false) }];
+}
+
+
+- (void)airplayChanged:(NSNotification *)sender
+{
+    AVAudioSessionRouteDescription* currentRoute = [[AVAudioSession sharedInstance] currentRoute];
+    BOOL isAirPlayPlaying = false;
+    for (AVAudioSessionPortDescription * output in currentRoute.outputs) {
+        if([output.portType isEqualToString:AVAudioSessionPortAirPlay]) {
+            isAirPlayPlaying = true;
+            break;
+        }
+    }
+    [self sendEventWithName:@"airplayConnected" body:@{@"connected": @(isAirPlayPlaying)}];
+}
+
+
+- (void) isAvailable;
+{
+    printf("init Available");
+    AVAudioSessionRouteDescription* currentRoute = [[AVAudioSession sharedInstance] currentRoute];
+    BOOL isAvailable = false;
+    int routeCount = (int)[[currentRoute outputs] count];
+    BOOL isSpeaker = ([[[[currentRoute outputs] objectAtIndex:0] portName] isEqualToString:@"Speaker"]);
+    if(routeCount > 0 &&!isSpeaker) {
+        isAvailable = true;
+    }
+    [self sendEventWithName:@"airplayAvailable" body:@{@"available": @(isAvailable)}];
+}
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"airplayAvailable", @"airplayConnected"];
+}
+
+
+@end
